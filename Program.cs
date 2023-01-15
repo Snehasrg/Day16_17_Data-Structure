8738a2189c6e6a1e72f0216c2be80d73a596150a
Snehasrg <sghongade642@gmail.com>
2023-01-15 13:12:20 +0530

    Method calling

--- Program.cs
+++ Program.cs
@@ -4,8 +4,13 @@
     {
         public static void Main(string[] args)
         {
-            InsertionSort.Display();
-           
+            Console.WriteLine("Welcome to DataStructure and Algorithm Practise Problem");
+
+            String str = "Best!";
+            int n = str.Length;
+            Console.Write("All possible strings of : ");
+            PermutationofString.usingIterativeMethod(str, 0, n - 1);
+
         }
     }
 }

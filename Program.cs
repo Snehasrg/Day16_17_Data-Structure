3845bb7b9e54eb0f711ace47a2ecc75d654189c7
Snehasrg <sghongade642@gmail.com>
2023-01-17 22:16:50 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -1,14 +1,12 @@
-﻿namespace AlgorithmProgram
+﻿using AlgorithmProgram;
+
+namespace AlgorithmProgram
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            BinarySearch d = new BinarySearch();
-            d.List();
-            Console.WriteLine("\nEnter word do you want to search : ");
-            string Word = Console.ReadLine();
-            d.Searching(Word);
+            InsertionSort.Display();
 
         }
     }

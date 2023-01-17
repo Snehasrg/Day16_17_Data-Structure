4334ea83c0651bc132cd8bdd8d43119b51a4dd3f
Snehasrg <sghongade642@gmail.com>
2023-01-17 22:29:51 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -6,9 +6,14 @@ namespace AlgorithmProgram
     {
         public static void Main(string[] args)
         {
-            BubbleSort bubbleSort = new BubbleSort();
-            bubbleSort.Display();
 
+            int[] arr = { 22, 11, 15, 5, 45, 7 };
+            Console.WriteLine("Given Array");
+            MergeSort.printArray(arr);
+            MergeSort obja = new MergeSort();
+            obja.sort(arr, 0, arr.Length - 1);
+            Console.WriteLine("\nSorted array");
+            MergeSort.printArray(arr);
 
         }
     }

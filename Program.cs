907069c974ed54fd91c7a441657d8c029af33ce7
Snehasrg <sghongade642@gmail.com>
2023-01-19 13:04:15 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -7,13 +7,7 @@ namespace AlgorithmProgram
         public static void Main(string[] args)
         {
 
-            int[] arr = { 22, 11, 15, 5, 45, 7 };
-            Console.WriteLine("Given Array");
-            MergeSort.printArray(arr);
-            MergeSort obja = new MergeSort();
-            obja.sort(arr, 0, arr.Length - 1);
-            Console.WriteLine("\nSorted array");
-            MergeSort.printArray(arr);
+            Anagram.Display();
 
         }
     }
